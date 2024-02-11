962ef9548ac15ee02c38847b68576b47ca8df32b
Naresh Kumar Mohana Sundaram <naresh@qmediax.com>
2024-02-11 17:03:38 +1100

    Added Swagger

--- EnlinqdSurveyManager/Program.cs
+++ EnlinqdSurveyManager/Program.cs
@@ -21,11 +21,11 @@ namespace EnlinqdSurveyManager
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
             app.UseAuthorization();
